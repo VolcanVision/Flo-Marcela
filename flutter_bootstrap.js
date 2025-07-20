594ae89d89a947f71f65aa4e33a36b772488fbe8
Devarsh-42 <devarshmehta2005@gmail.com>
2025-07-20 20:30:45 +0530

    A Hardcoded variables solution, have to be fixed in future

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1482919597"
+    serviceWorkerVersion: "2671290457"
   }
 });
