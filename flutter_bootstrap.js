c7d61f57a3cb6fca79359223c3481c6f808dcb06
Devarsh-42 <devarshmehta2005@gmail.com>
2025-07-20 23:47:21 +0530

    Optimized & Fixed An order management screen bug

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1369342408"
+    serviceWorkerVersion: "1414654063"
   }
 });
