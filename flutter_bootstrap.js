96a0d9306c668632425100e344e5aa88074e0926
Devarsh-42 <devarshmehta2005@gmail.com>
2025-07-24 00:12:34 +0530

    Production interface screen bugs fixed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1414654063"
+    serviceWorkerVersion: "1299242970"
   }
 });
